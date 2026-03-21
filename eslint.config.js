ccda1d9da25854a31d0a9c1c4349195f8bb52c6c
Nebojsa Ilic <nebojsailic@192.168.1.12>
2026-03-21 09:09:30 +0100

    install dependencies

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**']
+  },
+  {
+    files: ['src/**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...angular.configs.tsRecommended,
+      eslintConfigPrettier
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case'
+        }
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase'
+        }
+      ]
+    }
+  },
+  {
+    files: ['src/**/*.html'],
+    extends: [...angular.configs.templateRecommended, eslintConfigPrettier]
+  }
+);
